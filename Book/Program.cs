835fe104370b2342b9c13572305e3a3b9b73649e
AbramovDD <AbramovDD@206D-10>
2023-03-16 15:46:03 +0700

    Добавьте файлы проекта.

---
+++ Book/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooK_Title2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Check();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+
+    }
+}
